6bdb47086b2b37edabb02258760fa9bf50d50120
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-04-26 13:04:46 -0700

    Added a first draft for a connection manager.

---
+++ mongomodel/conn.py
@@ -0,0 +1,59 @@
+from twisted.internet import defer
+from twisted.python import log
+
+import txmongo
+
+class ConnectionManager(object):
+    """
+    """
+    def __init__(self):
+        _connection = None
+        _db = None
+        _collection = None
+
+
+    def setConnection(self, conn):
+        self._connection = conn
+        return self._connection
+
+    def getConnection(self):
+        if self._connection:
+            result = self._connection
+        else:
+            result = txmongo.MongoConnectionPool()
+        d = defer.maybeDeferred(result)
+        d.addErrback(log.err)
+        d.addCallback(setConnection)
+        return d
+
+    def setDB(self, ignored, dbName):
+        self._db = getattr(self._connection, dbName)
+        return self._db
+
+    def getDB(self, dbName):
+        if self._db:
+            result = self._db
+        elif self._connection:
+            result = self.setDB(dbName)
+        else:
+            result = self.getConnection()
+            result.addCallback(setDB, dbName)
+        d = defer.maybeDeferred(result)
+        d.addErrback(log.err)
+        return d
+
+    def setCollection(self, ignored, collName):
+        self._collection = getattr(self._db, collName)
+        return self._collection
+
+    def getCollection(self, dbName, collName):
+        if self._collection:
+            result = self._collection
+        else:
+            result = self.getDB(dbName)
+            result.addCallback(setCollection, collName)
+        d = defer.maybeDeferred(result)
+        d.addErrback(log.err)
+        return d
+            
+
